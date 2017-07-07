2f9a7c8deaea9b52659bec5b36697e9d1b69e3ec
Yang Nie <yang.nie@gmail.com>
2017-07-07 10:39:25 -0700

    style: remove useless code

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -19,7 +19,7 @@ from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
 
-# Create a Transformation matrics function
+# Create a Transformation matrix function
 def create_T_matrics(alpha, a, d, q):
     T_matrics = Matrix([[	    cos(q),		-sin(q),	     0,		a],
 		[sin(q)*cos(alpha),  cos(q)*cos(alpha),   -sin(alpha), -sin(alpha)*d],
@@ -62,81 +62,7 @@ def handle_calculate_IK(req):
 		'alpha6':           0,  'a6':      0, 'd7': 0.303, 'q7':       0}
 
 #	    rospy.loginfo("dh = %s" % dh)            
-            # Define Modified DH Transformation matrix
-	    T0_1 = Matrix([[		    cos(q1),		 -sin(q1),		0,		a0],
-			   [	sin(q1)*cos(alpha0),  cos(q1)*cos(alpha0),   -sin(alpha0), -sin(alpha0)*d1],
-			   [	sin(q1)*sin(alpha0),  cos(q1)*sin(alpha0),    cos(alpha0),  cos(alpha0)*d1],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T1_2 = Matrix([[		    cos(q2),		 -sin(q2),		0,		a1],
-			   [	sin(q2)*cos(alpha1),  cos(q2)*cos(alpha1),   -sin(alpha1), -sin(alpha1)*d2],
-			   [	sin(q2)*sin(alpha1),  cos(q2)*sin(alpha1),    cos(alpha1),  cos(alpha1)*d2],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T2_3 = Matrix([[		    cos(q3),		 -sin(q3),		0,		a2],
-			   [	sin(q3)*cos(alpha2),  cos(q3)*cos(alpha2),   -sin(alpha2), -sin(alpha2)*d3],
-			   [	sin(q3)*sin(alpha2),  cos(q3)*sin(alpha2),    cos(alpha2),  cos(alpha2)*d3],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T3_4 = Matrix([[		    cos(q4),		 -sin(q4),		0,		a3],
-			   [	sin(q4)*cos(alpha3),  cos(q4)*cos(alpha3),   -sin(alpha3), -sin(alpha3)*d4],
-			   [	sin(q4)*sin(alpha3),  cos(q4)*sin(alpha3),    cos(alpha3),  cos(alpha3)*d4],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T4_5 = Matrix([[		    cos(q5),		 -sin(q5),		0,		a4],
-			   [	sin(q5)*cos(alpha4),  cos(q5)*cos(alpha4),   -sin(alpha4), -sin(alpha4)*d5],
-			   [	sin(q5)*sin(alpha4),  cos(q5)*sin(alpha4),    cos(alpha4),  cos(alpha4)*d5],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T5_6 = Matrix([[		    cos(q6),		 -sin(q6),		0,		a5],
-			   [	sin(q6)*cos(alpha5),  cos(q6)*cos(alpha5),   -sin(alpha5), -sin(alpha5)*d6],
-			   [	sin(q6)*sin(alpha5),  cos(q6)*sin(alpha5),    cos(alpha5),  cos(alpha5)*d6],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-	    T6_G = Matrix([[		    cos(q7),		 -sin(q7),		0,		a6],
-			   [	sin(q7)*cos(alpha6),  cos(q7)*cos(alpha6),   -sin(alpha6), -sin(alpha6)*d7],
-			   [	sin(q7)*sin(alpha6),  cos(q7)*sin(alpha6),    cos(alpha6),  cos(alpha6)*d7],
-			   [			  0,  		 	0,   		0, 		 1]])
-
-#	    T0_1 = T0_1.subs(dh)
-#	    T1_2 = T1_2.subs(dh)
-#	    T2_3 = T2_3.subs(dh)
-#	    T3_4 = T3_4.subs(dh)
-#	    T4_5 = T4_5.subs(dh)
-#	    T5_6 = T5_6.subs(dh)
-#	    T6_G = T6_G.subs(dh)
-#	    rospy.loginfo("T6_G = %s" % T6_G) 
-
-            # Create individual transformation matrices
-#	    T0_2 = simplify(T0_1 * T1_2)
-#	    rospy.loginfo("T0_2 = %s" % T0_2)  
-#	    T0_3 = simplify(T0_2 * T2_3)
-#	    rospy.loginfo("T0_3 = %s" % T0_3)  
-#	    T0_4 = simplify(T0_3 * T3_4)
-#	    T0_5 = simplify(T0_4 * T4_5)
-#	    T0_6 = simplify(T0_5 * T5_6)
-#	    rospy.loginfo("T0_6 = %s" % T0_6)  
-#	    T0_G = simplify(T0_6 * T6_G)
-#	    rospy.loginfo("T0_G = %s" % T0_G)  
-
-	    # Define a correction between Gripper Link and DH convertion
-#	    R_z = Matrix([[	cos(np.pi),    -sin(np.pi),		0,	0],
-#			  [	sin(np.pi),	cos(np.pi),		0,	0],
-#			  [		 0,		 0,		1,	0],
-#			  [		 0,		 0,		0,	1]])
-
-#	    R_y = Matrix([[  cos(-np.pi/2),    		 0, sin(-np.pi/2),	0],
-#			  [		 0,		 1,		0,	0],
-#			  [ -sin(-np.pi/2),	         0, cos(-np.pi/2),	0],
-#			  [		 0,		 0,		0,	1]])	
-
-	    # Create a correction matrix
-#	    R_correct = simplify(R_z * R_y)
-
-	    # Get a final matrix to convert input postion paramters to output angle parameters
-#	    R_final = simplify(T0_G * R_correct)
-#	    rospy.loginfo("R_final = %s" % R_final)  
-            
+           
             # Extract end-effector position and orientation from request
 	    # px,py,pz = end-effector position
 	    # roll, pitch, yaw = end-effector orientation
@@ -144,29 +70,32 @@ def handle_calculate_IK(req):
             py = req.poses[x].position.y
             pz = req.poses[x].position.z    
 
+	    # calculate roll, pitch, yaw value
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 #     	    rospy.loginfo("Received %s eef-poses.orientation from the plan" % req.poses[x].orientation)
+
             # Calculate joint angles using Geometric IK method
 
     	    d6 = dh['d6']
     	    l =  dh['d7']
 
-#calculate l for x, y, z position
+	    # calculate l for x, y, z position
             lx = cos(roll) * cos(pitch)
             ly = sin(roll) * cos(pitch)
     	    lz = -sin(roll)
-#calculate  wrist center x, y, z position
+	    # calculate  wrist center x, y, z position
     	    Wx = px - (d6 + l) * lx 
     	    Wy = py - (d6 + l) * ly			
     	    Wz = pz - (d6 + l) * lz
 	    theta1 = atan2(Wy, Wx)
-	    rospy.loginfo("theta1 = %s" % theta1)	   
+#	    rospy.loginfo("theta1 = %s" % theta1)	   
 	    s = Wz - dh['d1']
 	    r = sqrt(Wx**2+Wy**2)
 	    rospy.loginfo("s = %s" % s)
 	    rospy.loginfo("r = %s" % r)
+	    # beta angle is the angle between Joint 2 and Joint 5
    	    beta = atan2(s, r)
     	    distance_c = sqrt(Wx**2+Wy**2+s**2)
 #	    print("d4 = %s" % dh['d4'])
@@ -179,9 +108,7 @@ def handle_calculate_IK(req):
 	    dh['q1'] = theta1
 	    dh['q2'] = theta2-np.pi/2
 	    dh['q3'] = theta3
-#	    dh['q4'] = 'q4'
-#	    dh['q5'] = 'q5'
-#	    dh['q6'] = 'q6'
+	    # DH table with q1, q2 and q3 value
 	    rospy.loginfo("dh = %s" % dh)  
 	    T0_1 = create_T_matrics(dh['alpha0'], dh['a0'], dh['d1'], dh['q1'])
 	    R0_1 = T0_1.extract([0,1,2],[0,1,2])
@@ -192,10 +119,11 @@ def handle_calculate_IK(req):
 	    T2_3 = create_T_matrics(dh['alpha2'], dh['a2'], dh['d3'], dh['q3'])
 	    R2_3 = T2_3.extract([0,1,2],[0,1,2])
 	#    rospy.loginfo("R2_3 = %s" % R2_3) 
+	    # create R0_3
 	    R0_3 = simplify(R0_1 * R1_2 * R2_3)
 	#    rospy.loginfo("R0_3 = %s" % R0_3) 
 
-# Build Rrpy
+	    # Build Rrpy
 	    R_roll = Matrix([[  1,	    0,	          0],
 			   [	0,  cos(roll),   -sin(roll)],
 			   [	0,  sin(roll),    cos(roll)]])
@@ -208,7 +136,7 @@ def handle_calculate_IK(req):
 	    Rrpy = simplify(R_roll * R_pitch * R_yaw)
 #	    rospy.loginfo("Rrpy = %s" % Rrpy) 
 
-# Calculate R3_6 using inv(R0_3) * Rrpy
+	    # Calculate R3_6 using inv(R0_3) * Rrpy
 	    R3_6 = simplify(R0_3.inv() * Rrpy)
 
 	    rospy.loginfo("R3_6 = %s" % R3_6) 
